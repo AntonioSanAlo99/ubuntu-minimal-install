4e6401f602aa2121c5c5c8b2c501ab2203f0ec97
antoniosanalo99 <111574729+AntonioSanAlo99@users.noreply.github.com>
2026-04-02 22:20:36 +0200

    Delete profiles directory

--- profiles/minimal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PACKAGES="\
-locales,console-setup,console-data,keyboard-configuration,\
-gettext,chrony,libpam-systemd,bash-completion,dbus-user-session,\
-linux-image-generic,linux-headers-generic,dracut,zstd,xz-utils,\
-wget,nano,efibootmgr,os-prober,ntfs-3g,network-manager,\
-${GRUB_PKG},sudo-rs,kmod,\
-iproute2,ca-certificates"
