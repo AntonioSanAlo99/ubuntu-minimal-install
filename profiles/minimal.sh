2b815ac6a7683bf204c20cf5ba2518f6595bc481
antoniosanalo99 <111574729+AntonioSanAlo99@users.noreply.github.com>
2026-04-02 20:19:55 +0200

    Update package list in minimal.sh
    Removed 'rust-coreutils' and 'util-linux' from the package list.

--- profiles/minimal.sh
+++ profiles/minimal.sh
@@ -5,5 +5,5 @@ locales,console-setup,console-data,keyboard-configuration,\
 gettext,chrony,libpam-systemd,bash-completion,dbus-user-session,\
 linux-image-generic,linux-headers-generic,dracut,zstd,xz-utils,\
 wget,nano,efibootmgr,os-prober,ntfs-3g,network-manager,\
-grub-pc,grub-efi,sudo-rs,rust-coreutils,util-linux,kmod,\
+grub-pc,grub-efi,sudo-rs,kmod,\
 iproute2,ca-certificates"
